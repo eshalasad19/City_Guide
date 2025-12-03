6f4e44ca33031daca7779b0c827b7c8a157332ab
asp <asp@aptech.com>
2025-12-03 12:58:31 +0500

    signup page

--- lib/signup.dart
+++ lib/signup.dart
@@ -41,7 +41,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+ TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController pswd = TextEditingController();
+  TextEditingController cpswd = TextEditingController();
+  TextEditingController age = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -51,9 +55,103 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body:Center(
         child: Column(
-      
-    )));
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Register Yourself", style: TextStyle(fontSize: 24)),
+            Container(
+              margin: EdgeInsets.all(10),
+              constraints: BoxConstraints(maxWidth: 300),
+              child: TextField(
+                controller: name,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter Your Name",
+                  suffixIcon: Icon(Icons.person),
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.all(10),
+
+              constraints: BoxConstraints(maxWidth: 300),
+              child: TextField(
+                controller: email,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter Your Email",
+                  suffixIcon: Icon(Icons.email),
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.all(10),
+
+              constraints: BoxConstraints(maxWidth: 300),
+              child: TextField(
+                controller: pswd,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter Your Password",
+                  suffixIcon: Icon(Icons.password),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+
+              constraints: BoxConstraints(maxWidth: 300),
+              child: TextField(
+                controller: cpswd,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Confirm Your Password",
+                  suffixIcon: Icon(Icons.password),
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.all(10),
+
+              constraints: BoxConstraints(maxWidth: 300),
+              child: TextField(
+                controller: age,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter Your Age",
+                  suffixIcon: Icon(Icons.cake),
+                ),
+              ),
+            ),
+
+            OutlinedButton.icon(
+              onPressed: () {
+                // add_user();
+              },
+              label: Text("Signup"),
+              icon: Icon(Icons.app_registration),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextButton(
+                onPressed: (){
+                  // Navigator.push(context, MaterialPageRoute(builder: (builder)=>eshal_login()));
+                }, child: Text("Already Have an Account", style: TextStyle(color: Colors.blue),)),
+            )
+          ],
+        ),
+    ));
   }
 }
