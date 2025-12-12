5971a709d1b0071ced0d2957edf8dc08cef19c2a
amalhisbani <amalhisbani@gmail.com>
2025-12-12 12:36:46 +0500

    attractionwork
    attraction, attractiondetail

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
+import 'package:citiguide/addattraction.dart';
 import 'package:citiguide/adminlogin.dart';
+import 'package:citiguide/attractiondetail.dart';
 import 'package:citiguide/citydetail.dart';
 import 'package:citiguide/firebase_options.dart';
 import 'package:citiguide/signup.dart';
@@ -169,6 +171,22 @@ class _MyHomePageState extends State<MyHomePage> {
               onTap: () {
                 Navigator.push(
                     context, MaterialPageRoute(builder: (s) => adminlogin()));
+              },
+            ),
+              ListTile(
+              leading: const Icon(Icons.admin_panel_settings),
+              title: const Text('add attraction '),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (s) => AddAttractionPage()));
+              },
+            ),
+             ListTile(
+              leading: const Icon(Icons.admin_panel_settings),
+              title: const Text('add attractiondetail'),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (s) => AddAttractionDetail()));
               },
             ),
           ],
